2a9184476437cefb8eef5839a7494b2043769cf0
Thibault Giraudon <thibaultgiraudon@MacBook-Air-de-Thibault.local>
2023-02-24 14:20:06 +0100

    update install.sh

--- install.sh
+++ install.sh
@@ -75,9 +75,3 @@ cd ~\n
 ./print_news/print_news ~/\$SUBJECT ~/\$COUNTRY >> ~/todays_news\n\
 cat ~/todays_news |& less" "$HOME" >> "$RC_FILE"
 fi
-
-# automatically replace current shell with new one.
-exec "$SHELL"
-
-printf "\033[33m... and don't forget, \033[1;37mpaco\033[0;33m is not a replacement for your own tests! \033[0m\n"
-
