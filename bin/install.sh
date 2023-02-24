19e0f285f4f226df7fc07049b18d4fbdd80eb052
Thibault Giraudon <thibaultgiraudon@MacBook-Air-de-Thibault.local>
2023-02-24 15:38:05 +0100

    update install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -12,6 +12,7 @@ git clone --recursive https://github.com/Thib1708/print_news.git
 
 cd ./print_news
 make 
+mkdir articles
 cd ..
 
 cp -r print_news "$HOME"
@@ -35,10 +36,10 @@ export COUNTRY=\"fr\"\n
 export API_KEY=\"\"\n
 export CITY=\"Lyon\"\n\n
 curl -s fr.wttr.in/\$CITY\\\?tpq > ~/todays_news\n\n
-curl https://newsapi.org/v2/everything -G -d q=\$SUBJECT -d sortBy=popularity -d apiKey=\$API_KEY> ~/\$SUBJECT\n\n
-curl https://newsapi.org/v2/top-headlines -G -d country=\$COUNTRY -d sortBy=popularity -d apiKey=\$API_KEY > ~/\$COUNTRY\n
+curl https://newsapi.org/v2/everything -G -d q=\$SUBJECT -d sortBy=popularity -d apiKey=\$API_KEY> ~/print_news/articles/\$SUBJECT\n\n
+curl https://newsapi.org/v2/top-headlines -G -d country=\$COUNTRY -d sortBy=popularity -d apiKey=\$API_KEY > ~/print_news/articles/\$COUNTRY\n
 cd ~\n
-./print_news/print_news ~/\$SUBJECT ~/\$COUNTRY >> ~/todays_news\n
+./print_news/print_news ./print_news/articles/\$SUBJECT ./print_news/articles/\$COUNTRY >> ~/todays_news\n
 cat ~/todays_news |& less" "$HOME" >> "$RC_FILE"
 printf "\033[1;32mcmds add\033[0m\n"
 printf "\033[48;5;5mopen ~/.zshrc and copy/paste your api_key in export API_KEY=\"\"\033[0m\n"
