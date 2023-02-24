68bca0aa0e985ab8daafc431550b22b59440b657
Thibault Giraudon <thibaultgiraudon@MacBook-Air-de-Thibault.local>
2023-02-24 14:26:42 +0100

    update install.sh

--- install.sh
+++ install.sh
@@ -68,10 +68,12 @@ if ! grep "SUBJECT=" "$RC_FILE" &> /dev/null; then
 export COUNTRY="fr"\n\
 export API_KEY="8290029380e4428bb679cc65052bcfc4"\n\
 export CITY="Lyon"\n\n\
-curl -s fr.wttr.in/\$CITY\?tpq > ~/todays_news\n\n\
+curl -s fr.wttr.in/\$CITY\\?tpq > ~/todays_news\n\n\
 curl https://newsapi.org/v2/everything -G -d q=\$SUBJECT -d sortBy=popularity -d apiKey=\$API_KEY> ~/\$SUBJECT\n\n\
 curl https://newsapi.org/v2/top-headlines -G -d country=\$COUNTRY -d sortBy=popularity -d apiKey=\$API_KEY > ~/\$COUNTRY\n\
 cd ~\n
 ./print_news/print_news ~/\$SUBJECT ~/\$COUNTRY >> ~/todays_news\n\
 cat ~/todays_news |& less" "$HOME" >> "$RC_FILE"
+echo "\033[1;32mcmds add"
+echo "\033[48;5;5mnow open ~/.zshrc and copy/paste your api_key in export API_KEY="""
 fi
