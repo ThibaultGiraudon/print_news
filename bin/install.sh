064c605b5f321706ae806a181bb3d4e5ad6302d3
Thib1708 <thibault.giraudon@gmail.com>
2023-02-25 21:51:02 +0100

    better installing process

--- bin/install.sh
+++ bin/install.sh
@@ -6,6 +6,7 @@ git clone --recursive https://github.com/Thib1708/print_news.git
 cd ./print_news
 make 
 mkdir articles
+chmod 751 ./bin/main.sh
 cd ..
 
 cp -r print_news "$HOME"
@@ -21,8 +22,8 @@ if ! grep "SUBJECT=" "$RC_FILE" &> /dev/null; then
 	printf "\nexport SUBJECT=\"Apple\"\n
 export COUNTRY=\"fr\"\n
 export API_KEY=\"\"\n
-export CITY=\"Lyon\"\n\n
-alias news=%s/print_news/bin/main.sh "$HOME" >> "$RC_FILE"
+export CITY=\"Lyon\"\n
+alias news=%s/print_news/bin/main.sh" "$HOME" >> "$RC_FILE"
 printf "\033[1;32mcmds add\033[0m\n"
 printf "\033[48;5;5mopen ~/.zshrc and copy/paste your api_key in export API_KEY=\"\"\033[0m\n"
 fi
